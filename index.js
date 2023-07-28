c45d60359e0cad74795219057745415d0cc1c217
GHOST-Aram <owinoojala@gmail.com>
2023-07-28 07:36:48 +0300

    Inital Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { 
+    matchedData, 
+    query, 
+    validationResult 
+} from 'express-validator'
+
+const app = express()
+app.use(express.json())
+
+app.get('/hello', query('person').notEmpty().escape(), (req, res) =>{
+    const result = validationResult(req)
+
+    if(result.isEmpty()){
+        const data = matchedData(req)
+        res.send(`Hello, ${data.person}!`)
+    }
+    else
+        res.send({errors: result.array()})
+})
+
+app.listen(3000)
