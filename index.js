b93bb3640bfb58ac4a557f62905512092f8e782f
GHOST-Aram <owinoojala@gmail.com>
2023-11-27 16:31:16 +0300

    chore: Validate nested request bodies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { 
+    body,
     matchedData, 
     query, 
     validationResult 
@@ -14,9 +15,58 @@ app.get('/hello', query('person').notEmpty().escape(), (req, res) =>{
     if(result.isEmpty()){
         const data = matchedData(req)
         res.send(`Hello, ${data.person}!`)
-    }
+    } 
     else
         res.send({errors: result.array()})
 })
+const subscriberToNewsletter = (req, res) =>{
+    /**subscribe to newsletter code */
+}
+app.post('/newsletter',
+    body('email')
+        .optional()
+        .trim()
+        .isEmail(),
+    subscriberToNewsletter
+)
+const createEmailChain = () => body('email').isEmail()
 
+app.post('/login', createEmailChain(), (req, res) =>{
+    res.login()
+})
+
+app.post('/signup', createEmailChain(), (req, res) =>{
+    /**register user */
+})
+
+// Nested request body
+const requestBody = {
+    "address": {
+        "home": { "number": 25 },
+        "work": { "number": 501 }
+    },
+    "siblings": [ 
+        { "name": "Maria vo Bismark" } ,
+        { "name": "Checky Bragface" }
+    ]
+}
+// Accessing nested request body by wildcards
+app.post('/update-user', 
+    body('address.*.number').isInt(),
+    body('siblings.*.name').notEmpty(),
+    (req, res) => {
+
+})
+
+// deeply Nested request body
+const teams = {
+    "name": "Team name",
+    "teams": [
+        {   "name": "Subteam name", "teams": [] }
+    ]
+}
+
+app.put('/update-chart', body('**.name').trim().notEmpty(),(req, res)=>{
+    /**request handling code */
+})
 app.listen(3000)
